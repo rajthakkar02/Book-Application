372fec3e0f0d0884e1dd3cad025127db4561688b
rajthakkar02 <rajthakkar0302@gmail.com>
2025-03-13 15:18:36 +0530

    Add feedback to user page

--- app/controllers/home_controller.rb
+++ app/controllers/home_controller.rb
@@ -5,6 +5,7 @@ class HomeController < ApplicationController
     elsif user_signed_in? && current_user.role == "seller"
     elsif user_signed_in? && current_user.role == "author"
       @books = Book.where(user_id: current_user.id).paginate(page: params[:page], per_page: 30)
+      @rating = Feedback.where(user_id: current_user.id).average(:rating).to_f
     else
       @q = BookSeller.ransack(params[:q])
       @books_seller = @q.result(distinct: true).paginate(page: params[:page], per_page: 30)
